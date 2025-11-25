79fd2c871ff89df576b8ef11ab2f1cec8278c8d1
taro <guiiilhermefspinto@gmail.com>
2025-11-25 17:26:07 +0000

    update

---
+++ Level01/rev_print.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+
+int ft_strlen(char *str)
+{
+    int i = 0;
+
+    while (str[i])
+        i++;
+    return (i);
+}
+
+
+int main(int argc, char *argv[])
+{
+    if (argc == 2)
+    {
+        int len = ft_strlen(argv[1]);
+
+        while (len > 0)
+        {
+            len--;
+            write(1, &argv[1][len], 1);
+        }
+    }
+    write(1, "\n", 1);
+    return (0);
+}
